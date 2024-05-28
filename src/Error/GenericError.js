fb2eb69683c07e0fcfe90c5ebfe77cb54be02c9f
jose.henrique <jose.henriquec523@gmail.com>
2024-05-28 19:24:01 -0300

    arrumar nome diretoria error

--- src/Error/GenericError.js
+++
@@ -1,9 +0,0 @@
-class GenericError extends Error {
-  constructor(message, options, genericOptions) {
-    super(message, options);
-    this.status = genericOptions.status;
-    this.genericOptions = { ...genericOptions };
-  }
-}
-
-export default GenericError;
