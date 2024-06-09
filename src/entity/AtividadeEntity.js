2c1ff78391047657415f99a9302492ba991ed5a5
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2024-06-09 18:16:45 -0300

    Artefato

--- src/entity/AtividadeEntity.js
+++ src/entity/AtividadeEntity.js
@@ -1,39 +1,133 @@
+import StatusEnum from "../enum/StatusEnum";
+import ArtefatoEntity from "./ArtefatoEntity";
+
 class AtividadeEntity {
-    constructor(atividade) {
-        if (atividade.id) this.id = atividade.id;
-        this.titulo = atividade.titulo;
-        this.descricao = atividade.descricao;
-        this.status = atividade.status;
-        this.dtEntrega = atividade.dtEntrega;
-        if (atividade.usuario_id) this.usuario_id = atividade.usuario_id;
-        if (atividade.atividade_pai_id) this.atividade_pai_id = atividade.atividade_pai_id;
+    constructor() {
+        this.titulo = "";
+        this.descricao = "";
+        this.status = StatusEnum.PENDENTE;
+        this.dtEntrega = null;
+        this.atividade_pai_id = null;
+        this.usuario_id = null;
+        this.artefato = null;
     }
 
-    static parse(atividade) {
-        const atividadeParse = {
-            titulo: atividade.titulo,
-            descricao: atividade.descricao,
-            status: atividade.status,
-            dtEntrega: atividade.dtEntrega,
-            usuario_id: atividade.usuario_id,
-            atividade_pai_id: atividade.atividade_pai_id
-        };
-
-        return new AtividadeEntity(atividadeParse);
+    getId() {
+        return this.id;
     }
 
-    static fromModel(atividadeModel) {
-        const atividadeParse = {
-            id: atividadeModel.id,
-            titulo: atividadeModel.titulo,
-            descricao: atividadeModel.descricao,
-            status: atividade.status,
-            dtEntrega: atividadeModel.dtEntrega,
-            usuario_id: atividadeModel.usuario_id,
-            atividade_pai_id: atividadeModel.atividade_pai_id,
-        };
+    setId(id) {
+        this.id = id;
+    }
 
-        return new AtividadeEntity(atividadeParse);
+    getTitulo() {
+        return this.titulo;
+    }
+
+    setTitulo(titulo) {
+        this.titulo = titulo;
+    }
+
+    getDescricao() {
+        return this.descricao;
+    }
+
+    setDescricao(descricao) {
+        this.descricao = descricao;
+    }
+
+    getStatus() {
+        return this.status;
+    }
+
+    setStatus(status) {
+        this.status = status;
+    }
+
+    getDtEntrega() {
+        return this.dtEntrega;
+    }
+
+    setDtEntrega(dtEntrega) {
+        this.dtEntrega = dtEntrega;
+    }
+
+    getUsuarioId() {
+        return this.usuario_id;
+    }
+
+    setUsuarioId(usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    getAtividadePaiId() {
+        return this.atividade_pai_id;
+    }
+
+    setAtividadePaiId(atividade_pai_id) {
+        this.atividade_pai_id = atividade_pai_id;
+    }
+
+    getArtefato() {
+        return this.getArtefato;
+    }
+
+    setArtefato(artefato) {
+        this.artefato = artefato;
+    }
+
+    toModel() {
+        return {
+            titulo: this.titulo,
+            descricao: this.descricao,
+            status: this.status,
+            dtEntrega: this.dtEntrega,
+            atividade_pai_id: this.atividade_pai_id,
+            usuario_id: this.usuario_id,
+            artefato: this.artefato
+        }
+    }
+
+    toJson() {
+        return {
+            id: this.id,
+            titulo: this.titulo,
+            descricao: this.descricao,
+            status: this.status,
+            dtEntrega: this.dtEntrega,
+            atividade_pai_id: this.atividade_pai_id,
+            usuario_id: this.usuario_id,
+            artefato: this.artefato
+        }
+    }
+
+    static fromModel(atividade) {
+        let atividadeEntity = new AtividadeEntity();
+        
+        atividadeEntity.setId(atividade.id);
+        atividadeEntity.setTitulo(atividade.titulo);
+        atividadeEntity.setDescricao(atividade.descricao);
+        atividadeEntity.setStatus(atividade.status);
+        atividadeEntity.setDtEntrega(atividade.dtEntrega);
+        atividadeEntity.setAtividadePaiId(atividade.atividade_pai_id);
+        atividadeEntity.setUsuarioId(atividade.usuario_id);
+        atividadeEntity.setArtefato(atividade.artefato?.map((artefato) => ArtefatoEntity.fromModel(artefato)));
+
+        return atividadeEntity
+    }
+
+    static fromJson(atividade) {
+        let atividadeEntity = new AtividadeEntity();
+        
+        atividadeEntity.setTitulo(atividade.titulo);
+        atividadeEntity.setDescricao(atividade.descricao);
+        atividadeEntity.setStatus(atividade.status);
+        atividadeEntity.setDtEntrega(atividade.dtEntrega);
+        atividadeEntity.setAtividadePaiId(atividade.atividade_pai_id);
+        atividadeEntity.setUsuarioId(atividade.usuario_id);
+        atividadeEntity.setArtefato(atividade.artefato?.map((artefato) => ArtefatoEntity.fromModel(artefato)));
+
+        return atividadeEntity
     }
 }
 
