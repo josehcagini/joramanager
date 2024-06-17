bc88658072d393d9f35fb1f1216da70bd3fdfda4
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2024-06-16 22:11:43 -0300

    Ajuste no controller

--- src/controller/ArtefatoController.js
+++ src/controller/ArtefatoController.js
@@ -4,183 +4,255 @@ import ArtefatoRepo from '../repository/ArtefatoRepo.js';
 import ArtefatoEntity from '../entity/ArtefatoEntity.js';
 
 class ArtefatoController {
-    async validarNovoArtefato(novoArtefato) {
-      if (!novoArtefato) {
-        throw new GenericError('corpo da requisicao nao possui artefato', { status: StatusCodes.BAD_REQUEST });
+  async validarNovoArtefato(novoArtefato) {
+    if (!novoArtefato) {
+      throw new GenericError('corpo da requisicao nao possui artefato', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novoArtefato.titulo) {
+      throw new GenericError('artefato nao possui titulo', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novoArtefato.descricao) {
+      throw new GenericError('artefato nao possui descricao', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novoArtefato.originalname) {
+      throw new GenericError('artefato nao possui nome original', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novoArtefato.filename) {
+      throw new GenericError('artefato nao possui nome de arquivo', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if(novoArtefato.url) {
+      throw new GenericError('artefato nao possui url', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    const artefatoExiste = await ArtefatoRepo.findOne({ where: { titulo: novoArtefato.titulo } });
+
+    if (artefatoExiste) {
+      throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
+    }
+  }
+
+  static async validarUpdateArtefato(artefatoUpdate) {
+    const ArtefatoExiste = await ArtefatoRepo.findOne({ where: { nome: artefatoUpdate.nome } });
+
+    if (ArtefatoExiste && ArtefatoExiste.id !== artefatoUpdate.artefatoId) {
+      throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
+    }
+
+    if (!artefatoUpdate.titulo) {
+      throw new GenericError('artefato nao possui titulo', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!artefatoUpdate.descricao) {
+      throw new GenericError('artefato nao possui descricao', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!artefatoUpdate.originalname) {
+      throw new GenericError('artefato nao possui nome original', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!artefatoUpdate.filename) {
+      throw new GenericError('artefato nao possui nome de arquivo', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if(artefatoUpdate.url) {
+      throw new GenericError('artefato nao possui url', { status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  static isRequestOther(artefatoRequest, artefatoGet) {
+    console.log('isRequestOther?', artefatoRequest !== artefatoGet);
+    return artefatoRequest !== artefatoGet;
+  }
+
+  static onlyHasAccessToSelf(permissoes) {
+    const retorno = (permissoes.length === 1
+      && !!permissoes.find((permissao) => [TipoOperacaoEnum.RETRIEVESELF, TipoOperacaoEnum.UPDATESELF, TipoOperacaoEnum.DELETESELF].includes(permissao.tipoOperacao)));
+
+    console.log('onlyHasAccessToSelf?', retorno);
+    return retorno;
+  }
+  
+  async store(req, res) {
+    try {
+      const { artefato } = req.body;
+
+      await ArtefatoController.validarNovoArtefato(artefato);
+
+      const artefatoSave = ArtefatoEntity.fromJson(artefato);
+
+      const novoArtefato = await ArtefatoRepo.create(artefatoSave);
+
+      const retjson = {
+        novoArtefato: {
+          id: novoArtefato.id,
+        },
+        paths: {
+          login: '/login',
+        },
+      };
+
+      return res.status(StatusCodes.CREATED).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+
+      return res.status(status).json({
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { artefatoId } = req.params;
+
+      if (!artefatoId) {
+        throw new GenericError('request sem artefatoId', { status: StatusCodes.BAD_REQUEST });
+      }
+      
+      if (
+        ArtefatoController.isRequestOther(req.artefato.id, artefatoId)
+        && ArtefatoController.onlyHasAccessToSelf(req.permissoes)
+      ) {
+        throw new GenericError('sem permissao', { status: StatusCodes.UNAUTHORIZED });
       }
 
-      if (!novoArtefato.titulo) {
-        throw new GenericError('artefato nao possui titulo', { status: StatusCodes.BAD_REQUEST });
+      const artefato = await ArtefatoRepo.findByPk(artefatoId);
+
+      const retjson = {
+        artefato,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const artefatos = await ArtefatoRepo.findAll();
+
+      const retjson = {
+        artefatos,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      console.log(error);
+
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { artefatoId } = req.params;
+      if (!artefatoId) {
+        throw new GenericError('artefatoId nao enviado', { status: StatusCodes.BAD_REQUEST });
       }
 
-      if (!novoArtefato.descricao) {
-        throw new GenericError('artefato nao possui descricao', { status: StatusCodes.BAD_REQUEST });
+      const artefato = await ArtefatoRepo.delete(artefatoId);
+
+      return res.status(StatusCodes.OK).json(artefato);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const artefatoId = Number(req.params.artefatoId);
+      if (!artefatoId) {
+        throw new GenericError('artefatoId nao enviado', { status: StatusCodes.BAD_REQUEST });
       }
 
-      if (!novoArtefato.originalname) {
-        throw new GenericError('artefato nao possui nome original', { status: StatusCodes.BAD_REQUEST });
+      const valuesUpdate = req.body;
+      if (!valuesUpdate) {
+        throw new GenericError('body vazio', { status: StatusCodes.BAD_REQUEST });
       }
 
-      if (!novoArtefato.filename) {
-        throw new GenericError('artefato nao possui nome de arquivo', { status: StatusCodes.BAD_REQUEST });
+      if (
+        ArtefatoController.isRequestOther(req.artefato.id, artefatoId)
+        && ArtefatoController.onlyHasAccessToSelf(req.permissoes)
+      ) {
+        throw new GenericError('sem permissao', { status: StatusCodes.UNAUTHORIZED });
       }
 
-      if(novoArtefato.url) {
-        throw new GenericError('artefato nao possui url', { status: StatusCodes.BAD_REQUEST });
-      }
-  
-      const artefatoExiste = await ArtefatoRepo.findOne({ where: { titulo: novoArtefato.titulo } });
-  
-      if (artefatoExiste) {
-        throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
-      }
+      await ArtefatoController.validarUpdateArtefato({ artefatoId, ...valuesUpdate });
+
+      const camposUpdate = ArtefatoEntity.fromJson(valuesUpdate);
+
+      const artefatoUpdate = await ArtefatoRepo.update(artefatoId, camposUpdate);
+
+      const retjson = {
+        artefato: artefatoUpdate,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
     }
-    
-    async store(req, res) {
-      try {
-        const { artefato } = req.body;
-  
-        await ArtefatoController.validarNovoArtefato(artefato);
-  
-        const artefatoSave = ArtefatoEntity.fromJson(artefato);
-  
-        const novoArtefato = await ArtefatoRepo.create(artefatoSave);
-  
-        const retjson = {
-          novoArtefato: {
-            id: novoArtefato.id,
-          },
-          paths: {
-            login: '/login',
-          },
-        };
-  
-        return res.status(StatusCodes.CREATED).json(retjson);
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-  
-        return res.status(status).json({
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        });
-      }
-    }
-  
-    async show(req, res) {
-      try {
-        const { artefatoId } = req.params;
-  
-        if (!artefatoId) {
-          throw new GenericError('request sem artefatoId', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const artefato = await ArtefatoRepo.findByPk(artefatoId);
-  
-        return res.status(StatusCodes.OK).json({ artefatos: artefato });
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
-    }
-  
-    async index(req, res) {
-      try {
-        const artefatos = await ArtefatoRepo.findAll();
-  
-        return res.status(StatusCodes.OK).json(artefatos);
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
-    }
-  
-    async delete(req, res) {
-      try {
-        const { artefatoId } = req.params;
-        if (!artefatoId) {
-          throw new GenericError('artefatoId nao enviado', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const artefato = await ArtefatoRepo.delete(artefatoId);
-  
-        return res.status(StatusCodes.OK).json(artefato);
-      } catch (error) {
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
-    }
-  
-    async update(req, res) {
-      try {
-        const { artefatoId } = { ...req.params };
-        if (!artefatoId) {
-          throw new GenericError('artefatoId nao enviado', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const valuesUpdate = { ...req.body };
-        if (!valuesUpdate) {
-          throw new GenericError('body vazio', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const camposUpdate = ArtefatoEntity.fromJson(valuesUpdate);
-  
-        const artefatoUpdate = await ArtefatoRepo.update(artefatoId, camposUpdate);
-  
-        console.log('retorno', artefatoUpdate);
-  
-        return res.status(StatusCodes.OK).json(artefatoUpdate);
-      } catch (error) {
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
-    }
+  }
 }
 
 export default new ArtefatoController();
--- src/controller/AtividadeController.js
+++ src/controller/AtividadeController.js
@@ -7,197 +7,273 @@ import StatusEnum from '../enum/StatusEnum.js';
 import ArtefatoController from './ArtefatoController.js';
 
 class AtividadeController {
-    static async validarNovaAtividade(novaAtividade) {
-      if (!novaAtividade) {
-        throw new GenericError('corpo da requisicao nao possui atividade', { status: StatusCodes.BAD_REQUEST });
-      }
-
-      if (!novaAtividade.titulo) {
-        throw new GenericError('atividade nao possui titulo', { status: StatusCodes.BAD_REQUEST });
-      }
-
-      if (!novaAtividade.descricao) {
-        throw new GenericError('atividade nao possui descricao', { status: StatusCodes.BAD_REQUEST });
-      }
-
-      if (!novaAtividade.dtentrega) {
-        throw new GenericError('atividade nao possui data de entrega', { status: StatusCodes.BAD_REQUEST });
-      }
-
-      if (!novaAtividade.usuario_id) {
-        throw new GenericError('atividade nao possui data de usuario', { status: StatusCodes.BAD_REQUEST });
-      }
-
-      if(!StatusEnum.isValidCode(novaAtividade.status)) {
-        throw new GenericError('atividade nao possui status', { status: StatusCodes.BAD_REQUEST });
-      }
-  
-      const atividadeExiste = await AtividadeRepo.findOne({ where: { titulo: novaAtividade.titulo } });
-  
-      if (atividadeExiste) {
-        throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
-      }
-      console.log("Teste 1")
-      if (novaAtividade.artefato) {
-        console.log("Teste 2")
-        for (const artefato of novaAtividade.artefato) {
-            await ArtefatoController.validarNovoArtefato(artefato);
-            console.log("Teste 3")
-        }
-      }
+  static async validarNovaAtividade(novaAtividade) {
+    if (!novaAtividade) {
+      throw new GenericError('corpo da requisicao nao possui atividade', { status: StatusCodes.BAD_REQUEST });
     }
 
-    async store(req, res) {
-      try {
-        const { atividade } = req.body;
-        console.log(atividade)
-        await AtividadeController.validarNovaAtividade(atividade);
-        console.log(atividade)
-        const atividadeSave = AtividadeEntity.fromJson(atividade);
-        console.log(atividade)
-        const novoAtividade = await AtividadeRepo.create(atividadeSave, {
-          include: [
-            {
-              association: "artefato",
-            },
-          ],
-        });
-  
-        const retjson = {
-          novoAtividade: {
-            id: novoAtividade.id,
-          },
-          paths: {
-            login: '/login',
-          },
-        };
-  
-        return res.status(StatusCodes.CREATED).json(retjson);
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-  
-        return res.status(status).json({
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        });
+    if (!novaAtividade.titulo) {
+      throw new GenericError('atividade nao possui titulo', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novaAtividade.descricao) {
+      throw new GenericError('atividade nao possui descricao', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novaAtividade.dtentrega) {
+      throw new GenericError('atividade nao possui data de entrega', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!novaAtividade.usuario_id) {
+      throw new GenericError('atividade nao possui data de usuario', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if(!StatusEnum.isValidCode(novaAtividade.status)) {
+      throw new GenericError('atividade nao possui status', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    const atividadeExiste = await AtividadeRepo.findOne({ where: { titulo: novaAtividade.titulo } });
+
+    if (atividadeExiste) {
+      throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
+    }
+    console.log("Teste 1")
+    if (novaAtividade.artefato) {
+      console.log("Teste 2")
+      for (const artefato of novaAtividade.artefato) {
+          await ArtefatoController.validarNovoArtefato(artefato);
+          console.log("Teste 3")
       }
     }
-  
-    async show(req, res) {
-      try {
-        const { atividadeId } = req.params;
-  
-        if (!atividadeId) {
-          throw new GenericError('request sem atividadeId', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const atividade = await AtividadeRepo.findByPk(atividadeId);
-  
-        return res.status(StatusCodes.OK).json({ atividades: atividade });
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
+  }
+
+  static async validarUpdateAtividade(atividadeUpdate) {
+    const atividadeExiste = await AtividadeRepo.findOne({ where: { nome: atividadeUpdate.nome } });
+
+    if (atividadeExiste && atividadeExiste.id !== atividadeUpdate.atividadeId) {
+      throw new GenericError('titulo ja em uso', { status: StatusCodes.CONFLICT });
     }
-  
-    async index(req, res) {
-      try {
-        const atividades = await AtividadeRepo.findAll();
-  
-        return res.status(StatusCodes.OK).json(atividades);
-      } catch (error) {
-        console.log(error);
-  
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
+
+    if (!atividadeUpdate) {
+      throw new GenericError('corpo da requisicao nao possui atividade', { status: StatusCodes.BAD_REQUEST });
     }
-  
-    async delete(req, res) {
-      try {
-        const { atividadeId } = req.params;
-        if (!atividadeId) {
-          throw new GenericError('atividadeId nao enviado', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const atividade = await AtividadeRepo.delete(atividadeId);
-  
-        return res.status(StatusCodes.OK).json(atividade);
-      } catch (error) {
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
+
+    if (!atividadeUpdate.titulo) {
+      throw new GenericError('atividade nao possui titulo', { status: StatusCodes.BAD_REQUEST });
     }
-  
-    async update(req, res) {
-      try {
-        const { atividadeId } = { ...req.params };
-        if (!atividadeId) {
-          throw new GenericError('atividadeId nao enviado', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const valuesUpdate = { ...req.body };
-        if (!valuesUpdate) {
-          throw new GenericError('body vazio', { status: StatusCodes.BAD_REQUEST });
-        }
-  
-        const camposUpdate = AtividadeEntity.fromJson(valuesUpdate);
-  
-        const atividadeUpdate = await AtividadeRepo.update(atividadeId, camposUpdate);
-  
-        console.log('retorno', atividadeUpdate);
-  
-        return res.status(StatusCodes.OK).json(atividadeUpdate);
-      } catch (error) {
-        const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
-        const retjson = {
-          error: {
-            message: error.message,
-            ...error,
-          },
-          paths: {
-            home: '/',
-          },
-        };
-        return res.status(status).json(retjson);
-      }
+
+    if (!atividadeUpdate.descricao) {
+      throw new GenericError('atividade nao possui descricao', { status: StatusCodes.BAD_REQUEST });
     }
+
+    if (!atividadeUpdate.dtentrega) {
+      throw new GenericError('atividade nao possui data de entrega', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if (!atividadeUpdate.usuario_id) {
+      throw new GenericError('atividade nao possui data de usuario', { status: StatusCodes.BAD_REQUEST });
+    }
+
+    if(!StatusEnum.isValidCode(novaAtividade.status)) {
+      throw new GenericError('atividade nao possui status', { status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  static isRequestOther(atividadeRequest, atividadeGet) {
+    console.log('isRequestOther?', atividadeRequest !== atividadeGet);
+    return atividadeRequest !== atividadeGet;
+  }
+
+  static onlyHasAccessToSelf(permissoes) {
+    const retorno = (permissoes.length === 1
+      && !!permissoes.find((permissao) => [TipoOperacaoEnum.RETRIEVESELF, TipoOperacaoEnum.UPDATESELF, TipoOperacaoEnum.DELETESELF].includes(permissao.tipoOperacao)));
+
+    console.log('onlyHasAccessToSelf?', retorno);
+    return retorno;
+  }
+
+  async store(req, res) {
+    try {
+      const { atividade } = req.body;
+      
+      await AtividadeController.validarNovaAtividade(atividade);
+      
+      const atividadeSave = AtividadeEntity.fromJson(atividade);
+      
+      const novoAtividade = await AtividadeRepo.create(atividadeSave, {
+        include: [
+          {
+            association: "artefato",
+          },
+        ],
+      });
+
+      const retjson = {
+        novaAtividade: {
+          id: novoAtividade.id,
+        },
+        paths: {
+          login: '/login',
+        },
+      };
+
+      return res.status(StatusCodes.CREATED).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+
+      return res.status(status).json({
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { atividadeId } = req.params;
+
+      if (!atividadeId) {
+        throw new GenericError('request sem atividadeId', { status: StatusCodes.BAD_REQUEST });
+      }
+      
+      if (
+        AtividadeController.isRequestOther(req.atividade.id, atividadeId)
+        && AtividadeController.onlyHasAccessToSelf(req.permissoes)
+      ) {
+        throw new GenericError('sem permissao', { status: StatusCodes.UNAUTHORIZED });
+      }
+
+      const atividade = await AtividadeRepo.findByPk(atividadeId);
+
+      const retjson = {
+        atividade,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      console.log(error);
+
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const atividades = await AtividadeRepo.findAll();
+
+      const retjson = {
+        atividades,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { atividadeId } = req.params;
+      if (!atividadeId) {
+        throw new GenericError('atividadeId nao enviado', { status: StatusCodes.BAD_REQUEST });
+      }
+
+      const atividade = await AtividadeRepo.delete(atividadeId);
+
+      return res.status(StatusCodes.OK).json(atividade);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const atividadeId = Number(req.params.atividadeId);
+      if (!atividadeId) {
+        throw new GenericError('atividadeId nao enviado', { status: StatusCodes.BAD_REQUEST });
+      }
+
+      const valuesUpdate = req.body;
+      if (!valuesUpdate) {
+        throw new GenericError('body vazio', { status: StatusCodes.BAD_REQUEST });
+      }
+
+      if (
+        AtividadeController.isRequestOther(req.atividade.id, atividadeId)
+        && AtividadeController.onlyHasAccessToSelf(req.permissoes)
+      ) {
+        throw new GenericError('sem permissao', { status: StatusCodes.UNAUTHORIZED });
+      }
+
+      await AtividadeController.validarUpdateAtividade({ atividadeId, ...valuesUpdate });
+
+      const camposUpdate = AtividadeEntity.fromJson(valuesUpdate);
+
+      const atividadeUpdate = await AtividadeRepo.update(atividadeId, camposUpdate);
+
+      const retjson = {
+        atividade: atividadeUpdate,
+        paths: {
+          home: '/',
+        },
+      };
+
+      return res.status(StatusCodes.OK).json(retjson);
+    } catch (error) {
+      const status = error.status ? error.status : StatusCodes.INTERNAL_SERVER_ERROR;
+      const retjson = {
+        error: {
+          message: error.message,
+          ...error,
+        },
+        paths: {
+          home: '/',
+        },
+      };
+      return res.status(status).json(retjson);
+    }
+  }
 }
 
 export default new AtividadeController();
